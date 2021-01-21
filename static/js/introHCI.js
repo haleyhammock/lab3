68e1f6c7c13a266ad51392a2bf0daca294d2b00c
Haley Hammock <hhammock@ucsd.edu>
2021-01-21 10:49:47 -0800

    Lab 3 changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -9,10 +9,34 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $('#testjs').click(function(e) {
+            $('.jumbotron h1').text("Javascript has taken control");
+            $("#testjs").text("Please wait...");
+            $(".jumbotron p").addClass("active");
+            $("a.thumbnail").click(projectClick);
+            });
+    $("a.thumbnail").click(projectClick);
+}
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+
+function projectClick(e) {
+	console.log("Project clicked");
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    //$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    //$("#description").hide()
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(description).hide()
+    }
 }
